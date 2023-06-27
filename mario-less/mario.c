1f784562745751f82b5dab33796aaf28cd1dcb08
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-06-27 15:37:23 +0000

    Tue, Jun 27, 2023, 11:37 PM +08:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,7 +1,22 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int get_height();
+//void draw();
+
+int main(void)
 {
-    
+    int h = get_height();
+    //draw(h);
+    printf("%i", h);
+}
+
+int get_height()
+{
+    int h;
+    do
+    {
+        h = get_int("Height: ");
+    }while (h < 1 || h > 8);
+    return h;
 }
