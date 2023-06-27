e20a7b3a01dea01db7b45ff3e97d45cab5482365
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-06-27 16:41:57 +0000

    Wed, Jun 28, 2023, 12:41 AM +08:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,46 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int get_height();
+void draw();
+
+int main(void)
 {
-    
+    int h = get_height();
+    draw(h);
+}
+
+int get_height()
+{
+    int h;
+    do
+    {
+        h = get_int("Height: ");
+    }while (h < 1 || h > 8);
+    return h;
+}
+
+void draw(int h)
+{
+    for(int i = 0; i < h; i++)
+    {
+        for (int j = 0; j < h-1-i; j++)
+        {
+            printf(" ");
+        }
+        for (int j = h-1-i; j<h; j++)
+        {
+            printf("#");
+        }
+        printf("  ");
+        for (int j = 0; j < i+1; j++)
+        {
+            printf("#");
+        }
+        for (int j = i+1; j < h; j++)
+        {
+            printf(".");
+        }
+        printf("\n");
+    }
 }
