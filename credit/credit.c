ec2a919cd43247a4e5e8edfff518422514982a33
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-06-28 09:36:38 +0000

    Wed, Jun 28, 2023, 5:36 PM +08:00

--- credit/credit.c
+++ credit/credit.c
@@ -3,8 +3,12 @@
 
 int checksum1();
 int checksum2();
-int get
+int get_number();
 int main(void)
 {
-
+    int number = get_number();
+    int checksum1 = checksum1();
+    int ckecksum2 = checksum2();
+    int checksum = checksum1 + checksum2();
+    
 }
