cf5b57c0360747efd2d0f8feab89ceeddeab97b2
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-06-29 11:36:25 +0000

    Thu, Jun 29, 2023, 7:36 PM +08:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -40,6 +40,9 @@ int compute_score(string word)
     int length = strlen(word);
     for (int i = 0; i < length; i++)
     {
-        if (islower())
+        if (islower(word[i]))
+        {
+            
+        }
     }
 }
