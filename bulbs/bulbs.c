8c997dc105aac495bbb9efe82b38bad6580079f7
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-06-29 15:18:07 +0000

    Thu, Jun 29, 2023, 11:18 PM +08:00

--- bulbs/bulbs.c
+++ bulbs/bulbs.c
@@ -23,7 +23,7 @@ int main(void)
             message[i] = quotient;
             bits[j] = remainders;
         }
-        for (int i = 0; i < 8; i++)
+        for (int l = 0; l < 8; l++)
         {
             print_bulb(bits[i]);
         }
