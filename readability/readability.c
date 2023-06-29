be3eadbd94b6809b8affff96e9bea8bdb77d1d42
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-06-29 12:44:49 +0000

    Thu, Jun 29, 2023, 8:44 PM +08:00

--- readability/readability.c
+++ readability/readability.c
@@ -16,5 +16,29 @@ int main(void)
     float L = letters/(float)words * 100;
     float S = sentences/(float)words *100;
     int index = (int)round(0.0588 * L - 0.296 * S - 15.8);
+    if (index >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else if (index < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else
+    {
+        printf("Grade %i\n", index);
+    }
+}
+
+int count_letters(string text)
+{
+
+}
+int count_words(string text)
+{
+
+}
+int count_sentences(string text)
+{
     
 }
