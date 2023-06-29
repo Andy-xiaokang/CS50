aac0c5cb11bf2ea8aae8c0b30bf22f8c1993648f
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-06-29 12:47:06 +0000

    Thu, Jun 29, 2023, 8:47 PM +08:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,6 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+
 #include <math.h>
 
 int count_letters(string text);
@@ -40,5 +41,5 @@ int count_words(string text)
 }
 int count_sentences(string text)
 {
-    
+
 }
