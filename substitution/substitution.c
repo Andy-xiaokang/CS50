8a2f361aa51cf91f1ce6166c9ac4a7f8fd6c12a8
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-06-30 01:32:16 +0000

    Fri, Jun 30, 2023, 9:32 AM +08:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,12 +1,19 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 int main(int argc, string argv[])
 {
-    while (argc < 2 || argc > 2)
+    if (argc < 2 || argc > 2)
     {
         printf("Usage: ./substitution key\n");
         return 1;
+    }
+    if (strlen(argv) != 26)
+    {
+        printf("Key must contain 26 characters.");
+        return 1
     }
     
 }
