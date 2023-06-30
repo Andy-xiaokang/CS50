ff91cb4e4fc0c4f9214d711c5d604a7463810f1a
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-06-30 01:47:22 +0000

    Fri, Jun 30, 2023, 9:47 AM +08:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -3,19 +3,21 @@
 #include <string.h>
 #include <ctype.h>
 
+string get_key(string array)
 int main(int argc, string argv[])
 {
+    int length =strlen(argv);
     if (argc < 2 || argc > 2)
     {
         printf("Usage: ./substitution key\n");
         return 1;
     }
-    if (strlen(argv) != 26)
+    if (length != 26)
     {
         printf("Key must contain 26 characters.");
         return 1;
     }
-    for (int i = 0; i < strlen(argv); i++)
+    for (int i = 0; i < length; i++)
     {
         if (!(isalpha(argv[i])))
         {
@@ -23,6 +25,14 @@ int main(int argc, string argv[])
         }
         return 1;
     }
+    for (int i = 0; i < length; i++)
+    {
+        for (j = i+1; j < length; j++)
+        {
+            
+        }
+    }
     string plaintext = get_string("plaintext: ");
-    
+    string key = get_key(argv);
+
 }
