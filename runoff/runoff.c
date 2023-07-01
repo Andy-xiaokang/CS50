d546cb76927faa8d89cdd6665d7b8bfaaa2bacee
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-01 11:37:47 +0000

    Sat, Jul 1, 2023, 7:37 PM +08:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -177,16 +177,6 @@ bool print_winner(void)
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
-    // int min = candidates[0].votes;
-    // for (int i = 0; i < candidate_count; i++)
-    // {
-    //     if (candidates[i].votes < min)
-    //     {
-    //         min = candidates[i].votes;
-    //     }
-    // }
-    // return min;
-
     for (int i = 0; i < candidate_count; i++)
     {
         if (candidates[i].eliminated)
