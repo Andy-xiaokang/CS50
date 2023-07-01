706313accd280caad7144191948776de78389bc9
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-01 03:04:36 +0000

    Sat, Jul 1, 2023, 11:04 AM +08:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -66,8 +66,7 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
-    // TODO
-    return false;
+    
 }
 
 // Print the winner (or winners) of the election
