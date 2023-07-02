29181e3026fc5a669ae133c8dce1568ab2412b8d
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-02 09:26:42 +0000

    Sun, Jul 2, 2023, 5:26 PM +08:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -165,14 +165,14 @@ void sort_pairs(void)
 void lock_pairs(void)
 {
     locked[pairs[0].winner][pairs[0].loser] = true;
-    // locked[pairs[1].winner][pairs[1].loser] = true;
-    for (int i = 0; i < pair_count; i++)
+    locked[pairs[1].winner][pairs[1].loser] = true;
+    for (int i = 2; i < pair_count; i++)
     {
         for (int j = 0; j < i - 1; j++)
         {
             if ( pairs[i].loser == pairs[j].winner )
             {
-                continue;
+                break;
             }
             else
             {
