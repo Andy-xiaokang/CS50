9ea22b9e83d51c70893731321ce7834de53bffb4
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-02 06:48:18 +0000

    Sun, Jul 2, 2023, 2:48 PM +08:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -117,7 +117,7 @@ void record_preferences(int ranks[])
     {
         for (j = i + 1; j < candidate_count; j++ )
         {
-            
+            preferences[ranks[i]][ranks[j]]++;
         }
     }
     return;
