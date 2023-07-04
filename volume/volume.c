c66420c851649a118ede25e4d5977d71bd6c2306
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-04 06:02:38 +0000

    Tue, Jul 4, 2023, 2:02 PM +08:00

--- volume/volume.c
+++ volume/volume.c
@@ -35,7 +35,6 @@ int main(int argc, char *argv[])
 
     // TODO: Copy header from input file to output file
     uint8_t header[HEADER_SIZE];
-
     fread(header, sizeof(uint8_t), HEADER_SIZE, input);
     fwrite(header, sizeof(uint8_t), HEADER_SIZE, output);
 
