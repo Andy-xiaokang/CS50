3b67c02c4da27ead74cb7ae7f77cb0f1d872f46f
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-04 14:21:56 +0000

    Tue, Jul 4, 2023, 10:21 PM +08:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -89,7 +89,6 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
     //         }
     //     }
     // }
-    tmp[0][]=
     for (int i = 1; i < height-1; i++)
     {
         for (int j = 1; j < width - 1; j++)
