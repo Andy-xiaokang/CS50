3cfc8ffc70b9c4bb00a261d0e54b7d8588c1be6c
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-04 09:19:07 +0000

    Tue, Jul 4, 2023, 5:19 PM +08:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -63,10 +63,11 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int i = 0; i < height; i++)
     {
+        RGBTRIBLE buffer;
         for (int j = 0; j < width/2; j++)
         {
-            RGBTRIBLE buffer;
             
+
         }
     }
     return;
