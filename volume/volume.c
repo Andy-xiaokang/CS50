daf39a43a7eada8651d9bbd2889b20df92b11c8b
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-04 03:30:09 +0000

    Tue, Jul 4, 2023, 11:30 AM +08:00

--- volume/volume.c
+++ volume/volume.c
@@ -46,12 +46,12 @@ int main(int argc, char *argv[])
     fwrite(header, sizeof(uint8_t), HEADER_SIZE, output);
 
     // TODO: Read samples from input file and write updated data to output file
-    int16_t buffer;
-    while(fread(&buffer, sizeof(int16_t), 1, input+HEADER_SIZE))
-    {
-        buffer *= factor;
-        fwrite(&buffer, sizeof(int16_t), 1, output+HEADER_SIZE);
-    }
+    // int16_t buffer;
+    // while(fread(&buffer, sizeof(int16_t), 1, input+HEADER_SIZE))
+    // {
+    //     buffer *= factor;
+    //     fwrite(&buffer, sizeof(int16_t), 1, output+HEADER_SIZE);
+    // }
 
     // Close files
     fclose(input);
