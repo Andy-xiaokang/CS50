9207aa73fee7a071d835fc672f9be38538cce8d7
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-04 03:17:44 +0000

    Tue, Jul 4, 2023, 11:17 AM +08:00

--- volume/volume.c
+++ volume/volume.c
@@ -46,7 +46,10 @@ int main(int argc, char *argv[])
 
     // TODO: Read samples from input file and write updated data to output file
     int16_t buffer;
-    
+    while(fread(&buffer, sizeof(int16_t), 1, input+HEADER_SIZE))
+    {
+        buffer *=2;
+    }
 
     // Close files
     fclose(input);
