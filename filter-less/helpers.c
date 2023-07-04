679dc9edb7b04b9091da450872749e205c67e6de
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-04 12:10:54 +0000

    Tue, Jul 4, 2023, 8:10 PM +08:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -22,7 +22,7 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             int sepiaRed, sepiaGreen, sepiaBlue;
-            sepiaRed = (int) round(0.396*image[i][j].rgbtRed + 0.769*image[i][j].rgbtGreen + 0.189*image[i][j].rgbtBlue);
+            sepiaRed = (int) round(0.393*image[i][j].rgbtRed + 0.769*image[i][j].rgbtGreen + 0.189*image[i][j].rgbtBlue);
             sepiaGreen = (int) round(0.349*image[i][j].rgbtRed + 0.686*image[i][j].rgbtGreen + 0.168*image[i][j].rgbtBlue);
             sepiaBlue = (int) round(0.272*image[i][j].rgbtRed + 0.534*image[i][j].rgbtGreen + 0.131*image[i][j].rgbtBlue);
             if (sepiaRed > 255)
