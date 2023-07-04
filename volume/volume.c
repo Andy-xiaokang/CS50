9cef15fac7a5c520f5086dd1da6d7bce049ac696
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-04 03:05:42 +0000

    Tue, Jul 4, 2023, 11:05 AM +08:00

--- volume/volume.c
+++ volume/volume.c
@@ -34,8 +34,12 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
-    uint8_t header[44];
-    
+    uint8_t header[HEADER_SIZE];
+    for (int i = 0; i < HEADER_SIZE; i++)
+    {
+        
+    }
+
 
     // TODO: Read samples from input file and write updated data to output file
 
