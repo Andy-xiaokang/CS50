cbee9ec0d4afaeb3caf14c54e0faf2e5506e5e45
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-04 13:05:59 +0000

    Tue, Jul 4, 2023, 9:05 PM +08:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -77,6 +77,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIP
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
@@ -87,7 +88,7 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
             }
             else
             {
-                
+
             }
         }
     }
