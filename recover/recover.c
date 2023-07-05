83fd726fa3bd1201a636a0e81fd7e97cd1de4bfe
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-05 07:32:09 +0000

    Wed, Jul 5, 2023, 3:32 PM +08:00

--- recover/recover.c
+++ recover/recover.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
     {
         if (buffer[0] == 0xff && buffer[1] == 0xdf && buffer[2] == 0xff && (buffer[3]&0xf0) == 0xe0)
         {
-            filename = sprintf(filename, "%03i.jpg", count);
+            sprintf(filename, "%03i.jpg", count);
             output = fopen(filename, "w");
             fwrite(buffer, 1, BLOCK_SIZE, output);
             count++;
