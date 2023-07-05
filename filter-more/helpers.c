c32be1b69e27a9143b9d33da807cfa91edaeae76
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-05 05:31:16 +0000

    Wed, Jul 5, 2023, 1:31 PM +08:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -76,51 +76,55 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 }
 
 // Detect edges
+//reference from a very concise and precise way to calculate
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
-    RGBTRIPLE tmp[height+2][width+2];
-    for (int i = 0; i < height+2; i++)
-    {
-        for (int j = 0; j < width+2; j++)
-        {
-            if (i == 0 || i == height + 2 - 1 || j == 0 || j== width + 2 - 1)
-            {
-                tmp[i][j].rgbtRed = tmp[i][j].rgbtGreen = tmp[i][j].rgbtBlue = 0;
-            }
-            else
-            {
-                tmp[i+1][j+1] = image[i][j];
-            }
-        }
-    }
-    RGBTRIPLE buffer[height+2][width+2];
-    for (int i = 1; i < height; i++)
-    {
-        for (int j = 1; j < width; j++)
-        {
-            int gxr, gxg, gxb;
-            int gyr, gyg, gyb;
-            gxr = tmp[i-1][j-1].rgbtRed*(-1)+ 0 + tmp[i-1][j+1].rgbtRed + tmp[i][j-1].rgbtRed*(-2) + 0 + tmp[i][j+1].rgbtRed*2 + tmp[i+1][j-1].rgbtRed*(-1) + 0 + tmp[i+1][j+1].rgbtRed*1;
-            gxg = tmp[i-1][j-1].rgbtGreen*(-1)+ 0 + tmp[i-1][j+1].rgbtGreen + tmp[i][j-1].rgbtGreen*(-2) + 0 + tmp[i][j+1].rgbtGreen*2 + tmp[i+1][j-1].rgbtGreen*(-1) + 0 + tmp[i+1][j+1].rgbtGreen*1;
-            gxb = tmp[i-1][j-1].rgbtBlue*(-1)+ 0 + tmp[i-1][j+1].rgbtBlue + tmp[i][j-1].rgbtBlue*(-2) + 0 + tmp[i][j+1].rgbtBlue*2 + tmp[i+1][j-1].rgbtBlue*(-1) + 0 + tmp[i+1][j+1].rgbtBlue*1;
+    RGBTRIPLE tmp[height][width];
+    int Gx[9] = {-1, 0, 1, -2, 0, 2, -1, 0, 1};
+    int Gy[9] = {-1, -2, -1, 0, 0, 0, 1, 2, 1};
 
-            gyr = tmp[i-1][j-1].rgbtRed*(-1)+ tmp[i-1][j].rgbtRed*(-2) + tmp[i-1][j+1].rgbtRed*(-1) + 0 + tmp[i+1][j-1].rgbtRed*(1)+ tmp[i+1][j].rgbtRed*(2) + tmp[i+1][j+1].rgbtRed*(1);
-            gyg = tmp[i-1][j-1].rgbtGreen*(-1)+ tmp[i-1][j].rgbtGreen*(-2) + tmp[i-1][j+1].rgbtGreen*(-1) + 0 + tmp[i+1][j-1].rgbtGreen*(1)+ tmp[i+1][j].rgbtGreen*(2) + tmp[i+1][j+1].rgbtGreen*(1);
-            gyb = tmp[i-1][j-1].rgbtBlue*(-1)+ tmp[i-1][j].rgbtBlue*(-2) + tmp[i-1][j+1].rgbtBlue*(-1) + 0 + tmp[i+1][j-1].rgbtBlue*(1)+ tmp[i+1][j].rgbtBlue*(2) + tmp[i+1][j+1].rgbtBlue*(1);
-            double a = round(sqrt(gxr*gxr+gyr*gyr));
-            double b = round(sqrt(gxg*gxg+gyg*gyg));
-            double c = round(sqrt(gxb*gxb+gyb*gyb));
-
-            buffer[i][j].rgbtRed =  a > 255 ? 255 : a;
-            buffer[i][j].rgbtGreen = b > 255 ? 255 : b;
-            buffer[i][j].rgbtBlue = c > 255 ? 255 : c;
-        }
-    }
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            image[i][j] = buffer[i+1][j+1];
+            int rx = 0, ry = 0;
+            int gx = 0, gy = 0;
+            int bx = 0, by = 0;
+            int index = -1;
+            for (int row = -1; row <= 1; row++ )
+            {
+                for (int col = -1; col <=1; col++)
+                {
+                    index++;
+                    if (i + row < 0 || i + row > height - 1)
+                    {
+                        continue;
+                    }
+                    if (j + col < 0 || j + col > height - 1)
+                    {
+                        continue;
+                    }
+                    rx += Gx[index]*image[i+row][j+col].rgbtRed;
+                    ry += Gy[index]*image[i+row][j+col].rgbtRed;
+
+                    gx += Gx[index]*image[i+row][j+col].rgbtGreen;
+                    gy += Gy[index]*image[i+row][j+col].rgbtGreen;
+
+                    bx += Gx[index]*image[i+row][j+col].rgbtBlue;
+                    by += Gy[index]*image[i+row][j+col].rgbtBlue;
+                }
+            }
+            tmp[i][j].rgbtRed = round(sqrt(rx*rx+ry*ry)) > 255 ? 255 : round(sqrt(rx*rx+ry*ry));
+            tmp[i][j].rgbtGreen = round(sqrt(gx*gx+gy*gy)) > 255 ? 255 : round(sqrt(gx*gx+gy*gy));
+            tmp[i][j].rgbtBlue = round(sqrt(bx*bx+by*by)) > 255 ? 255 : round(sqrt(bx*bx+by*by));
+        }
+    }
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j] = tmp[i][j];
         }
     }
     return;
