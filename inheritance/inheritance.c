ed9171cddcf87afff768476babe3a87e43fed2de
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-06 08:27:36 +0000

    Thu, Jul 6, 2023, 4:27 PM +08:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -54,7 +54,7 @@ person *create_family(int generations)
         root->parents[1] = parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-        
+        root->alleles[0] = parent0
 
     }
 
