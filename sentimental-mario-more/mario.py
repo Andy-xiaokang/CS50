f0b0bc38101b360bede60cfe0a55ab8972f299b9
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-08 08:34:56 +0000

    Sat, Jul 8, 2023, 4:34 PM +08:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,1 +1,12 @@
- 
+def main():
+    height = get_height()
+    draw(height)
+
+
+def get_height()
+    while True:
+        try:
+            h = int(input("Height: "))
+            if h > 0:
+                return h
+        ValueError: 
