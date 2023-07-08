80bc0b5a11c0202783d1a6a5f605859433546d3f
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-08 12:31:58 +0000

    Sat, Jul 8, 2023, 8:31 PM +08:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -3,4 +3,12 @@ def main():
     letters = count_letters(text)
     words = count_words(text)
     sentences = count_sentences(text)
-    L = (letters/)
+    L = (letters/words) * 100
+    S = (sentences/words) * 100
+    index = round(0.0588 * L - 0.296 * S - 15.8)
+    if index >= 16:
+        print("Grade 16+")
+    elif index < 1:
+        print("Before Grade 1")
+    else
+        print(index)
