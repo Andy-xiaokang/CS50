23c7346359992b204eb82bbe0913500e54891eae
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-09 03:01:21 +0000

    Sun, Jul 9, 2023, 11:01 AM +08:00

--- dna/dna.py
+++ dna/dna.py
@@ -6,10 +6,10 @@ def main():
 
     # TODO: Check for command-line usage
     if len(sys.argv) != 3:
-        print()
-
+        print("Usage: python dna.py data.csv sequence.txt")
 
     # TODO: Read database file into a variable
+    
 
     # TODO: Read DNA sequence file into a variable
 
