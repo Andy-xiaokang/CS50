0a5c534387a6695fbe84f46eebd6194c8c4b269a
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-09 03:17:11 +0000

    Sun, Jul 9, 2023, 11:17 AM +08:00

--- dna/dna.py
+++ dna/dna.py
@@ -16,9 +16,11 @@ def main():
             datas.append(row)
 
     # TODO: Read DNA sequence file into a variable
-    
+    with open(sys.argv[2],'r') as file:
+        dna = file.read()
 
     # TODO: Find longest match of each STR in DNA sequence
+    
 
     # TODO: Check database for matching profiles
 
