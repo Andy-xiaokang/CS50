5e197e6b9cda1f96be610e1855bfe9e6d90d8143
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-10 16:03:45 +0000

    Tue, Jul 11, 2023, 12:03 AM +08:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,1 +1,1 @@
-SELECT title, rating FROM movies INNER JOIN ratings ON movies.id = ratings.movie_id ORDER BY rating DESC, title;
+SELECT title, rating FROM movies JOIN ratings ON movies.id = ratings.movie_id ORDER BY rating DESC, title;
