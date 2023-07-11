0ab701c678e2fce3abb1b4b70a86c00307d39de0
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-11 06:24:43 +0000

    Tue, Jul 11, 2023, 2:24 PM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -8,8 +8,8 @@ SELECT account_number FROM atm_transactions WHERE year = 2021 AND month = 7 AND 
 SELECT caller FROM phone_calls WHERE year = 2021 AND month = 7 AND day = 28 AND duration < 60;
 SELECT passport_number FROM passengers WHERE flight_id IN (SELECT id FROM flights WHERE origin_airport_id IN (SELECT id FROM airports WHERE city ='Fiftyville'));
 -- earliest flight id in 2021.7.29 is 36  and the airports id of Fiftyville is 8
-select * from flights where origin_airport_id = 8 and year = 2021 AND month = 7 AND day = 29;
-select id from flights where origin_airport_id = 8 and year = 2021 AND month = 7 AND day = 29 AND hour = 8 AND minute = 20;
+select * from flights where origin_airport_id = 8 and year = 2021 AND month = 7 AND day = 29;  --earliest time is 8:20 am
+select id from flights where origin_airport_id = 8 and year = 2021 AND month = 7 AND day = 29 AND hour = 8 AND minute = 20;  --flight_id is 36
 
 SELECT name FROM people WHERE license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND 15<minute AND minute<=25 AND activity = 'exit') ORDER BY name;
 --Barry Bruce Diana Iman Kelsey Luca Sofia Vanessa
@@ -19,4 +19,6 @@ SELECT name FROM people WHERE id IN (SELECT person_id FROM bank_accounts WHERE a
 --Benista Brooke Bruce Diana Iman Kenny Luca Taylor
 select name from people where passport_number in (select passport_number FROM passengers WHERE flight_id = 36) ORDER BY name;
 --Bruce Doris Edward Kelsey Kenny Luca Sofia Taylor
+
+
 
