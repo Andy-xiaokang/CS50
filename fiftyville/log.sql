a4e2f338cb499c4057d2be60863c5d36e7f6db60
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-11 04:32:20 +0000

    Tue, Jul 11, 2023, 12:32 PM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,7 +1,5 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 sqlite3 fiftyville.db
 .schema
-
 SELECT description FROM crime_scene_reports WHERE year = 2021 AND month = 7 AND day = 28 AND street = 'Humphrey Street';
-
 SELECT transcript FROM interviews WHERE year = 2021 AND month = 7 AND day = 28;
