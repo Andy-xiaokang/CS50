06104ba8f76e5634fba22e9f65eb31a6783cf225
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-11 04:39:44 +0000

    Tue, Jul 11, 2023, 12:39 PM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -3,3 +3,5 @@ sqlite3 fiftyville.db
 .schema
 SELECT description FROM crime_scene_reports WHERE year = 2021 AND month = 7 AND day = 28 AND street = 'Humphrey Street';
 SELECT transcript FROM interviews WHERE year = 2021 AND month = 7 AND day = 28;
+
+SELECT activity, license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND 5<minute<15;
