644afb1085bd72348464182fcd1c1d0a439f9abe
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-11 06:41:45 +0000

    Tue, Jul 11, 2023, 2:41 PM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -22,7 +22,7 @@ select name from people where passport_number in (select passport_number FROM pa
 --Bruce Doris Edward Kelsey Kenny Luca Sofia Taylor
 
 select city from airports where id = (select destination_airport_id from flights where id = 36);
---
+--city: New York City
 
 SELECT receiver FROM phone_calls WHERE (caller = (select phone_number from people where name = 'Bruce')) AND year = 2021 AND month = 7 AND day = 28 AND duration < 60;
 --receiver: (375) 555-8161
