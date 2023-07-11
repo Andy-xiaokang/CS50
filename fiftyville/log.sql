563755a31c8489611566c2daa45474930892ddbf
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-11 04:51:56 +0000

    Tue, Jul 11, 2023, 12:51 PM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -5,3 +5,4 @@ SELECT description FROM crime_scene_reports WHERE year = 2021 AND month = 7 AND 
 SELECT transcript FROM interviews WHERE year = 2021 AND month = 7 AND day = 28;
 
 SELECT activity, license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND 5<minute<15 AND activity = 'exit';
+SELECT account_number, amount FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND transaction_type = 'withdraw';
