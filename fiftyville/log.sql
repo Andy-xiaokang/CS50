b65bb196ffeda941baf326ebc5773b7017874937
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-11 05:23:20 +0000

    Tue, Jul 11, 2023, 1:23 PM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -4,7 +4,7 @@ sqlite3 fiftyville.db
 SELECT description FROM crime_scene_reports WHERE year = 2021 AND month = 7 AND day = 28 AND street = 'Humphrey Street';
 SELECT transcript FROM interviews WHERE year = 2021 AND month = 7 AND day = 28;
 SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND 15<minute<=25 AND activity = 'exit';
-SELECT account_number, amount FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND transaction_type = 'withdraw';
+SELECT account_number FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND transaction_type = 'withdraw';
 SELECT caller FROM phone_calls WHERE year = 2021 AND month = 7 AND day = 28 AND duration < 60;
 
 SELECT name FROM people WHERE license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND 15<minute<=25 AND activity = 'exit');
