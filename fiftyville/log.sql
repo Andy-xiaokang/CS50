cbfc2c7cf5f305d00937054b5527033cc0d66403
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-11 04:12:22 +0000

    Tue, Jul 11, 2023, 12:12 PM +08:00

---
+++ fiftyville/log.sql
@@ -0,0 +1,7 @@
+-- Keep a log of any SQL queries you execute as you solve the mystery.
+sqlite3 fiftyville.db
+.schema
+
+SELECT description FROM crime_scene_reports WHERE year = 2021 AND month = 7 AND day = 28 AND street = 'Humphrey Street';
+
+SELECT activity, license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute = 15;
