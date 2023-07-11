4354ea2697a3f514da330b91250866b6582c87c1
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-11 05:22:17 +0000

    Tue, Jul 11, 2023, 1:22 PM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -8,5 +8,6 @@ SELECT account_number, amount FROM atm_transactions WHERE year = 2021 AND month 
 SELECT caller FROM phone_calls WHERE year = 2021 AND month = 7 AND day = 28 AND duration < 60;
 
 SELECT name FROM people WHERE license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND 15<minute<=25 AND activity = 'exit');
+SELECT name FROM people WHERE phone_number IN (SELECT caller FROM phone_calls WHERE year = 2021 AND month = 7 AND day = 28 AND duration < 60);
 
 
