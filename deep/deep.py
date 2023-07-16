635ad92e6c068b9945a8d676c1744337ce2cba46
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-16 13:30:54 +0000

    Sun, Jul 16, 2023, 9:30 PM +08:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,2 +1,10 @@
 def main():
-    if input() == ''
+    s = input('What is the answer to the Great Question of Life, the Universe and Everything')
+    if s == '42':
+        print('Yes')
+    elif s.lower() == 'forty-two' or s.lower() == 'forty two':
+        print('Yes')
+    else:
+        print('No')
+
+main()
