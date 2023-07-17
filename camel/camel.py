cc3d8e736137ad7433098c16ab1cae4ce85aef9b
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-17 11:27:31 +0000

    Mon, Jul 17, 2023, 7:27 PM +08:00

--- camel/camel.py
+++ camel/camel.py
@@ -1,3 +1,8 @@
 def main():
     camel = input("camelCase: ")
-    for 
+    after = ''
+    for s in camel:
+        if s.islower():
+            after = after + s
+        elif s.isupper():
+            after = after + 
