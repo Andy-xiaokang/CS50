39178bb8938dfdb4cc5a9285124590af5e3ce437
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-17 02:40:16 +0000

    Mon, Jul 17, 2023, 10:40 AM +08:00

---
+++ extensions/extensions.py
@@ -0,0 +1,13 @@
+def main():
+    media_type = {
+        'gif': 'image/gif',
+        'jpg': 'image/jpg',
+        'jpeg': 'image/jpeg',
+        'png': 'image/png',
+        'pdf': 'application/pdf',
+        'txt': 'text/plain',
+        'zip': 'application/zip'
+    }
+    s = input('File name: ').lower().split('.')[1]
+    if s in media_type:
+        print('')
