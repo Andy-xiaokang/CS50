9d85e50e89bb0487f83e2e1ca8e506ef3621e624
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-17 11:55:00 +0000

    Mon, Jul 17, 2023, 7:55 PM +08:00

--- coke/coke.py
+++ coke/coke.py
@@ -2,5 +2,15 @@ def main():
     amount = 50
     while amount > 0:
         print("Amount Due:", amount)
-        input = get_input()
-        
+        insert = get_input()
+        amount = amount - insert
+    print("Change Owed:",0-amount)
+
+def get_input():
+    while True:
+        insert = int(input("Insert Coin: "))
+        if insert == 5 or insert == 10 or insert == 25:
+            break
+    return insert
+
+main()
