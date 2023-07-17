495c6572b1b1b0482c4eb3674fe250397fbc560e
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-17 13:39:40 +0000

    Mon, Jul 17, 2023, 9:39 PM +08:00

--- plates/plates.py
+++ plates/plates.py
@@ -18,7 +18,8 @@ def is_valid(s):
     for i in s:
         if i.isdigit():
             index = s.find(i)
-            if s[index] == '0':
+            break
+    if s[index] == '0':
                 return False
     if not s[index:].isdigit():
         return False
