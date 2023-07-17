f919dfc035ffbcdf7f02258d6c9868bef7b96333
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-17 02:45:22 +0000

    Mon, Jul 17, 2023, 10:45 AM +08:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,14 +1,14 @@
 def main():
     media_type = {
         'gif': 'image/gif',
-        'jpg': 'image/jpg',
+        'jpg': 'image/jpeg',
         'jpeg': 'image/jpeg',
         'png': 'image/png',
         'pdf': 'application/pdf',
         'txt': 'text/plain',
         'zip': 'application/zip'
     }
-    s = input('File name: ').lower().strip().split('.')[]
+    s = input('File name: ').lower().strip().split('.')[-1]
     if s in media_type:
         print(media_type[s])
     else:
