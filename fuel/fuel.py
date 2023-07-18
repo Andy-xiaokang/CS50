f2a4509a84b328bb213e1d7eed3d346f132e9908
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-18 07:27:57 +0000

    Tue, Jul 18, 2023, 3:27 PM +08:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,8 +1,11 @@
 def main():
-    fuel("Fraction: ")
-
+    frac = fuel("Fraction: ")
+    print(str(frac)+'%')
 
 def fuel(s):
     try:
-        input_string = 
+        x,y = int(input(s).split('/')[0]) , int(input(s).split('/')[0])
+        return round((x/y)*100)
+    except (ValueError, ZeroDivisionError):
+        pass
 
