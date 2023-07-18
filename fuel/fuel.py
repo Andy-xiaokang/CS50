b1f66e5aa447792c316c5cc5cbd1320efbd1258f
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-18 07:38:16 +0000

    Tue, Jul 18, 2023, 3:38 PM +08:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -2,15 +2,20 @@ def main():
     frac = fuel("Fraction: ")
     if frac <= 1:
         print('E')
-    elif frac 
-    print(str(frac)+'%')
+    elif frac  100:
+        print('F')
+    else:
+        print(str(frac)+'%')
 
 def fuel(s):
     while True:
         try:
             input_string = input(s).split('/')
             x,y = int(input_string[0]) , int(input_string[1])
-            return round((x/y)*100)
+            if round((x/y)*100) > 100:
+                continue
+            else:
+                return round((x/y)*100)
         except (ValueError, ZeroDivisionError):
             pass
 
