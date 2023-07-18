b0dc507fe5517e5c7dc0b0f8f449b14447f980fa
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-18 11:53:04 +0000

    Tue, Jul 18, 2023, 7:53 PM +08:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -5,6 +5,7 @@ def main():
             if int(date[0]) > 12 or int(date[1]) > 31:
                 continue
             else:
-                print(f"{date[2]:04}{date[0]:02}{date[1]:02}")
-                
+                print(f"{date[2]:04}-{date[0]:02}-{date[1]:02}")
 
+
+main()
