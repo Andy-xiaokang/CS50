46fe3e69a2fda85efe1a843c7be9abf191d21f36
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-18 12:10:49 +0000

    Tue, Jul 18, 2023, 8:10 PM +08:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -8,9 +8,9 @@ def main():
                 print(f"{int(date[2]):04}-{int(date[0]):02}-{int(date[1]):02}")
                 return 0
         else:
-            date = date[0]
+            date = date[0].split(',')
             while True:
-                year = date.split
+                if len(date) 
 
 
 main()
