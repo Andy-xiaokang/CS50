7d1c3a7210d2852906e7dcfb190d3bde80dcba0e
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-18 08:52:49 +0000

    Tue, Jul 18, 2023, 4:52 PM +08:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -1,5 +1,13 @@
 def main():
     dict = {}
-    
+    while True:
+        try:
+            item = input().upper()
+            if item in dict:
+                dict[''] += 1
+            else:
+                dict['']
+        except EOFError:
+            break
 
 main()
