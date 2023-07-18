91d8749ee70ca817e33cf1766b4f82536b9ee700
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-18 11:52:12 +0000

    Tue, Jul 18, 2023, 7:52 PM +08:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -2,5 +2,9 @@ def main():
     while True:
         date = input("Date: ").split('/')
         if len(date == 3):
-            
+            if int(date[0]) > 12 or int(date[1]) > 31:
+                continue
+            else:
+                print(f"{date[2]:04}{date[0]:02}{date[1]:02}")
+                
 
