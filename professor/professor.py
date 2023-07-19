85097e5482fa2e9ea4f98d0fa2e4f034b882713c
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-19 12:27:30 +0000

    Wed, Jul 19, 2023, 8:27 PM +08:00

--- professor/professor.py
+++ professor/professor.py
@@ -6,9 +6,15 @@ def main():
     level = get_level()
     for i in range(10):
         x,y = generate_integer(level), generate_integer()
-        print(f"{x} + {y} =", input())
+        print(f"{x} + {y} =", end = '')
+        answer = input()
         try:
-            if x + y == int
+            if x + y == int(answer):
+                score += 1
+                continue
+            else:
+                
+
 
 
 def get_level():
