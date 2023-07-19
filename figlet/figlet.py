eafa968b0d52713a0c23ac60ab2fe1619502c4df
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-19 09:19:43 +0000

    Wed, Jul 19, 2023, 5:19 PM +08:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,4 +1,5 @@
 import sys
+import random
 from pyfiglet import Figlet
 
 figlet = Figlet()
@@ -6,8 +7,19 @@ fonts = figlet.getFonts()
 
 if len(sys.argv) == 1:
     s = input("Input: ")
-    
+    f = random.choice(fonts)
+    figlet.setFont(font=f)
+    print("Output: ")
+    print(figlet.renderText(s))
 elif len(sys.argv) == 3:
+    if sys.argv[1] != '-f' or sys.argv[1] != '--font' or sys.argv[2] not in fonts:
+        sys.exit("Argument error")
+    s = input("Input: ")
+    f = sys.argv[2]
+    figlet.setFont(font=f)
+    print("Output: ")
+    print(figlet.renderText(s))
+
 
 else:
     sys.exit("Invalid argument")
