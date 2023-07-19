bb0c0d841376f251f15dfdf0cd3e6666973411de
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-19 11:35:41 +0000

    Wed, Jul 19, 2023, 7:35 PM +08:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -1,9 +1,15 @@
 import inflect
 
 p = inflect.engine()
-mylist = p.join(["apple", "banana"])
-print(mylist)
+
 names = []
 
 while True:
-    
+    try:
+        name = input("name")
+        names.append(name)
+    except EOFError:
+        break
+
+mylist = p.join(names)
+print("Adieu, adieu, to " + mylist)
