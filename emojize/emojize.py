9acc2cb46d9e3f275aaaf4c4e95122952309fbb4
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-19 08:53:34 +0000

    Wed, Jul 19, 2023, 4:53 PM +08:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -1,11 +1,8 @@
-# from emoji import emojize
-
-# def main():
-#     str = input("Input: ")
-#     print("Output: " + emojize(str, language='alias'))
-
-
-# main()
 from emoji import emojize
 
-print("Output:", emojize(input("Input: ")))
+def main():
+    str = input("Input: ")
+    print("Output: " + emojize(str, language='alias'))
+
+
+main()
