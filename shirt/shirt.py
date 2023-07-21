89b97ff502a97609bc39fd5bdfabd16ff076cb43
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-21 06:50:00 +0000

    Fri, Jul 21, 2023, 2:50 PM +08:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -7,4 +7,12 @@ if len(sys.argv) < 3:
 elif len(sys.argv) > 3:
     sys.exit("Too many command-line arguments")
 
-input_extension = 
+input_extension = os.path.splitext(sys.argv[1])[1]
+output_extension = os.path.splitext(sys.argv[2])[1]
+
+if input_extension not in [".jpg",".jpeg",".png"] or output_extension not in [".jpg",".jpeg",".png"]:
+    sys.exit("Invalid input")
+
+if input_extension != output_extension:
+    sys.exit("Input and output have different extensions")
+
