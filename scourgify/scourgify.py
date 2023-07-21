d9a0c44a5bfa5ed14ed2688beb31e387cbe27bf9
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-21 04:52:08 +0000

    Fri, Jul 21, 2023, 12:52 PM +08:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -1,3 +1,14 @@
 import sys
 import csv
 
+if len(sys.argv) < 3:
+    sys.exit("Too few command-line argument")
+elif len(sys.argv) > 3:
+    sys.exit("Too many command-line argument")
+else:
+    try:
+        with open(sys.argv[1], "r") as csvfile:
+            dictreader = csv.DictReader(csvfile)
+            
+    except FileNotFoundError:
+        sys.exit(f"Could not read {sys.argv[1]}")
