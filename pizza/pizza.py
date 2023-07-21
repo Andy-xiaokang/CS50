1021f7c2a9a498f4639879947914648c08722cf2
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-21 03:20:35 +0000

    Fri, Jul 21, 2023, 11:20 AM +08:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -1,3 +1,15 @@
 import sys
 from tabulate import tabulate
 import csv
+
+
+if len(sys.argv) < 2:
+    sys.exit("Too few command line arguments")
+elif len(sys.argv) > 2:
+    sys.exit("Too many command line arguments")
+else:
+    if not sys.argv[1].endswith(".csv"):
+        sys.exit("Not a csv file")
+    try:
+        with open(sys.argv[1], "r") as file:
+            reader = 
