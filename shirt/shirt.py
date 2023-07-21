db5d72971a73e62c2f434f1de5829ff8fc19b3a1
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-21 06:31:24 +0000

    Fri, Jul 21, 2023, 2:31 PM +08:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -1,1 +1,10 @@
 import sys
+from PIL import image
+
+if len(sys.argv) < 3:
+    sys.exit("Too few command-line arguments")
+elif len(sys.argv) > 3:
+    sys.exit("Too many command-line arguments")
+else:
+    if (not argv[1].endswith(".jpg") and not argv[1].endswith(".jpeg") and not argv[1].endswith(".png")) or (not argv[2].endswith(".jpg") and not argv[2].endswith(".jpeg") and not argv[2].endswith(".png")):
+        
