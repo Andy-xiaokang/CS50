64e1c7fbf06f8fed0ddbf140c6c7a77c848a3eeb
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-21 02:41:37 +0000

    Fri, Jul 21, 2023, 10:41 AM +08:00

--- lines/lines.py
+++ lines/lines.py
@@ -5,7 +5,10 @@ elif len(sys.argv) > 2:
     sys.exit("Too many command line arguments")
 else:
     filename = sys.argv[1]
+    if not filename.endwith(".py"):
+        sys.exit("Not a python file")
     try:
         with open(filename, "r") as file:
+            
     except FileNotFoundError:
         sys.exit("File does not exist")
