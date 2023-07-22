d38d0f42407c9b59c45c2e7455a450e15ad0fab5
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-22 08:13:50 +0000

    Sat, Jul 22, 2023, 4:13 PM +08:00

--- working/working.py
+++ working/working.py
@@ -23,6 +23,21 @@ def convert(s):
         pmh = pmh + 12
         return f"{amh:02}:{amm:02} to {pmh:02}:{pmm:02}"
 
+    matchs = re.search(r"(\d?\d):?(\d\d)? PM to (\d?\d):?(\d\d)? AM", s)
+    if matchs:
+        pmh, pmm, amh, amm = matchs.group(1), matchs.group(2), matchs.group(3), matchs.group(4)
+        if amm == None:
+            amm = '0'
+        if pmm == None:
+            pmm = '0'
+        amh, amm, pmh, pmm = int(amh), int(amm), int(pmh), int(pmm)
+        if not 1 <= amh <= 12 or not 1 <= pmh <= 12:
+            raise ValueError
+        if not 0 <= amm <= 59 or not 0 <= pmm <= 59:
+            raise ValueError
+        pmh = pmh + 12
+        return f"{amh:02}:{amm:02} to {pmh:02}:{pmm:02}"
+
     else:
         raise ValueError
 
