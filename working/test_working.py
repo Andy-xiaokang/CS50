3c154722104db30904c07504d53228af80df7f17
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-22 08:46:31 +0000

    Sat, Jul 22, 2023, 4:46 PM +08:00

--- working/test_working.py
+++ working/test_working.py
@@ -1,4 +1,5 @@
 from working import convert
+import pytest
 
 def test_amtopm():
     assert convert("9 AM to 5 PM") == "09:00 to 17:00"
@@ -13,4 +14,16 @@ def test_bound():
     assert convert("12:00 AM to 12:00 PM") == "00:00 to 12:00"
 
 def test_invalidinput():
-    
+    with pytest.raises(ValueError):
+        convert("8:60 AM to 4:60 PM")
+    with pytest.raises(ValueError):
+        convert("9AM to 5PM")
+    with pytest.raises(ValueError):
+        convert("09:00 to 17:00")
+    with pytest.raises(ValueError):
+        convert("9 AM - 5 PM")
+    with pytest.raises(ValueError):
+        convert("10:7 AM - 5:1 PM")
+    with pytest.raises(ValueError):
+        convert("13:00 AM to 15:90 PM")
+
