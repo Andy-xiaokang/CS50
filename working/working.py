a6871d168a2b5468fc53825f1e9cb5689c877ac8
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-22 07:44:20 +0000

    Sat, Jul 22, 2023, 3:44 PM +08:00

--- working/working.py
+++ working/working.py
@@ -8,7 +8,9 @@ def main():
 
 
 def convert(s):
-    matchs = re.search(r"(\d?\d):?(\d\d)? AM to (\d?\d):?()? PM", s)
+    matchs = re.search(r"(\d?\d):?(\d\d)? AM to (\d?\d):?(\d\d)? PM", s)
+    if matchs:
+        amh, amm, pmh, pmm = 
 
 
 
