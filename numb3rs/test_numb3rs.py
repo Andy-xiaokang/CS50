a261b680a85085e5a878edc332e5885e94ef0521
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-22 05:39:40 +0000

    Sat, Jul 22, 2023, 1:39 PM +08:00

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -11,6 +11,8 @@ def test_normal():
 def test_stepbound():
     assert validate("512.512.512.512") == False
     assert validate("1.2.3.1000") == False
+def test_firstbyte():
+    assert validate("2001:0db8:85a3:0000:0000:8a2e:0370:7334") == False
 
 def test_invalidinput():
     assert validate("10.10.10.10.10") == False
