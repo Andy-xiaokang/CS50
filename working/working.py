6190a380adf7fcaf6bbddc5a4411c86d6f5fdce7
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-22 07:56:38 +0000

    Sat, Jul 22, 2023, 3:56 PM +08:00

--- working/working.py
+++ working/working.py
@@ -11,9 +11,12 @@ def convert(s):
     matchs = re.search(r"(\d?\d):?(\d\d)? AM to (\d?\d):?(\d\d)? PM", s)
     if matchs:
         amh, amm, pmh, pmm = matchs.group(1), matchs.group(2), matchs.group(3), matchs.group(4)
-        if amm == None and pmm == None:
-
-            amh, amm, pmh, pmm = int(amh), int(amm), int(pmh), int(pmm)
+        if amm == None:
+            amm = '0'
+        if pmm == None:
+            pmm = '0'
+        amh, amm, pmh, pmm = int(amh), int(amm), int(pmh), int(pmm)
+        if not 1 <= amh <= 12 or not 1 <= pmh <= 12
 
     else:
         raise ValueError
