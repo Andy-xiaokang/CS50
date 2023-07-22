390f26c4d7e86163672b8f4e6ce68562b0c85134
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-22 07:51:01 +0000

    Sat, Jul 22, 2023, 3:51 PM +08:00

--- working/working.py
+++ working/working.py
@@ -12,6 +12,7 @@ def convert(s):
     if matchs:
         amh, amm, pmh, pmm = matchs.group(1), matchs.group(2), matchs.group(3), matchs.group(4)
         if amm != None and pmm != None:
+            amh, amm, pmh, pmm = int(amh), int(amm), int(pmh), int(pmm)
             
     else:
         raise ValueError
