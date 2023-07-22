732203ac3a2bc34c7e4951ce65abd5a8895e8322
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-22 07:53:24 +0000

    Sat, Jul 22, 2023, 3:53 PM +08:00

--- working/working.py
+++ working/working.py
@@ -11,9 +11,10 @@ def convert(s):
     matchs = re.search(r"(\d?\d):?(\d\d)? AM to (\d?\d):?(\d\d)? PM", s)
     if matchs:
         amh, amm, pmh, pmm = matchs.group(1), matchs.group(2), matchs.group(3), matchs.group(4)
-        if amm != None and pmm != None:
+        if amm == None and pmm == None:
+
             amh, amm, pmh, pmm = int(amh), int(amm), int(pmh), int(pmm)
-            
+
     else:
         raise ValueError
 
