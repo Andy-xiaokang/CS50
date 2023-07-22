f0f273b4a3c4df19d9c556486e89cbef1df26748
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-22 09:18:08 +0000

    Sat, Jul 22, 2023, 5:18 PM +08:00

--- um/test_um.py
+++ um/test_um.py
@@ -1,4 +1,11 @@
 from um import count
 import pytest
 
-def test
+def test_case():
+    assert count("um") == 1
+    assert count("UM") == 1
+def test_word():
+    assert count("um... ") == 1
+    assert count("hello um world") == 1
+    assert count("this is um cs50") == 1
+    assert count("Um? Mum? Is this that album where, um, umm, the clumsy alums play drums?") == 2
