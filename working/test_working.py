3e1c6c3906c96c15c22370669d88173e09322a18
Andy <39621697+Andy-xiaokang@users.noreply.github.com>
2023-07-22 08:40:24 +0000

    Sat, Jul 22, 2023, 4:40 PM +08:00

---
+++ working/test_working.py
@@ -0,0 +1,16 @@
+from working import convert
+
+def test_amtopm():
+    assert convert("9 AM to 5 PM") == "09:00 to 17:00"
+    assert convert("9:00 AM to 5:00 PM") == "09:00 to 17:00"
+
+def test_pmtoam():
+    assert convert("8 PM to 8 AM") == "20:00 to 08:00"
+    assert convert("8:00 PM to 8:00 AM") == "20:00 to 08:00"
+
+def test_bound():
+    assert convert("12 AM to 12 PM") == "00:00 to 12:00"
+    assert convert("12:00 AM to 12:00 PM") == "00:00 to 12:00"
+
+def test_invalidinput():
+    
